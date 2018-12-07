f4082bfd7d705e27b2e81d0a0b2894d9f820f0f0
ghostlypi <parth.iyer@gmail.com>
2018-12-07 12:04:30 -0800

    Update GameController.js

--- javascript/GameController.js
+++ javascript/GameController.js
@@ -18,8 +18,8 @@ class GameController {
           new Block(this, new Vector3D(0, 0, 0), new Vector3D(10, 0.5, 20), "0xaaaaaa", "textures/wood_texture.jpg"), //floor
           new Block(this, new Vector3D(0, -0.1, 0), new Vector3D(800, 0.5, 800), "0xaaaaaa", "textures/iron_texture.jpg"), //ground
           //Walls
-          new Block(this, new Vector3D(0, 10, 8), new Vector3D(20, 20, 0.5), "0xaaaaaa", "textures/iron_texture.jpg"), 
-          new Block(this, new Vector3D(0, 10, -8), new Vector3D(10, 20, 0.5), "0xaaaaaa", "textures/wood_texture.jpg"),
+          new Block(this, new Vector3D(0, 10, 10), new Vector3D(10, 20, 0.5), "0xaaaaaa", "textures/wood_texture.jpg"), 
+          new Block(this, new Vector3D(0, 10, -10), new Vector3D(10, 20, 0.5), "0xaaaaaa", "textures/wood_texture.jpg"),
           new Block(this, new Vector3D(0, 10, 0), new Vector3D(0.5, 20, 10), "0xaaaaaa", "textures/wood_texture.jpg"),
           new Block(this, new Vector3D(0, 20, 0), new Vector3D(10, 0.5, 20), "0xaaaaaa", "textures/wood_texture.jpg"),//celing
           //new Block(this, new Vector3D(2, 0, 2), new Vector3D(2, 0.4, 2), "0xaaaaaa", "textures/iron_texture.jpg", (obj) => { obj.pos.y = 10 * Math.sin(performance.now() / 5000 + 3) + 10.1; obj.vel.y = 20/10000 * Math.cos(performance.now() / 5000 + 3); } ), //elevator
